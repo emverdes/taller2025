da2c3fbda943b30419d6c3d58e600a352b5ecfd2
Enrique Verdes <emverdes@gmail.com>
2025-02-17 22:15:33 -0300

    Creamos inventario con variables

---
+++ simple-script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Uso de disco"
+df -h
+echo "====="
+echo "Uptime del servidor"
+uptime -p
+echo "====="
+if [ -e /etc/debian_version ]; then
+	exit
+fi
+echo "Estado de SELinux"
+sestatus | grep '^Selinux status'
+echo "=FIN="
+
